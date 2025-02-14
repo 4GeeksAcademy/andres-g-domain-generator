a1c389f073006845ea801f296afb9f020a9de7bb
Andrés Guaipo <andresguaipo.16@gmail.com>
2025-02-14 20:53:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["my", "the"];
+let adj = ["ugly", "big"];
+let noun = ["apple", "ugly"];
+
+let x = 0;
+let y = 0;
+let z = 0;
+
+for (x; x < pronoun.length; x++) {
+  for (y; y < adj.length; y++) {
+    for (z; z < noun.length; z++) {
+      console.log(pronoun[x] + adj[y] + noun[z] + ".com");
+    }
+  }
+}
